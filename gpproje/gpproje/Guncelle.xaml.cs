9eb306c3c3b8df772024471a4888b0a5dc72505d
Elvan Ekilsin <78863009+eelvoon@users.noreply.github.com>
2022-05-31 18:55:49 +0300

    project added.

---
+++ gpproje/gpproje/Guncelle.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace gpproje
+{
+    /// <summary>
+    /// Guncelle.xaml etkileşim mantığı
+    /// </summary>
+    public partial class Guncelle : Window
+    {
+        public Guncelle()
+        {
+            InitializeComponent();
+            List<Ogrenciler> Kayit = new List<Ogrenciler>();
+            Kayit.Add(new Ogrenciler() { ID = 1, Adı = "Elvan", Soyadı = "Ekilsin", Alis = new DateTime(2022, 7, 23), Telefon = 05455645});
+            Kayit.Add(new Ogrenciler() { ID = 2, Adı = "Ecrin", Soyadı = "Elif", Alis = new DateTime(2022, 1, 17), Telefon = 05445454});
+            Kayit.Add(new Ogrenciler() { ID = 3, Adı = "Yusuf", Soyadı = "Arslan", Alis = new DateTime(2022, 11, 20), Telefon = 05446454});
+            Kayit.Add(new Ogrenciler() { ID = 4, Adı = "Nazlı", Soyadı = "Akış", Alis = new DateTime(2022, 1, 17), Telefon = 05454534});
+          
+
+            KayitliOgr.ItemsSource = Kayit;
+        }
+
+        public class Ogrenciler
+        {
+            public int ID { get; set; }
+
+            public string Adı { get; set; }
+
+            public string Soyadı { get; set; }
+
+            public string mail { get; set; }
+
+            public int Telefon { get; set; }
+
+            public DateTime Alis { get; set; }
+
+            public DateTime Teslim { get; set; }
+
+        }
+    }
+}
---
+++ gpproje/gpproje/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+namespace gpproje
+{
+  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (ad.Text == "" || soyad.Text == "" || tc.Text == "" || comboBox1.Text == "")
+            {
+
+                MessageBox.Show("Eksik girildi lütfen tam doldurunuz.", "HATA", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            else
+            {
+                listbox1.Items.Add("Adınız:" +" "+ad.Text + "   " + "Soyadınız:" +" "+ soyad.Text + "     " + "Tc:" +" "+ tc.Text + "   " + "Yaş:" +" "+ comboBox1.Text);
+
+                ad.Text = "";
+                soyad.Text = "";
+                tc.Text = "";
+                comboBox1.Text = "";
+
+                ad.Focus();
+
+            }
+
+
+        }
+
+
+        private void Temizle_click(object sender, RoutedEventArgs e)
+        {
+
+
+            ad.Clear();
+            soyad.Clear();
+            tc.Clear();
+            comboBox1.SelectedIndex = -1;
+
+
+
+
+        }
+
+        DialogResultConverter DialogResultConverter = new DialogResultConverter();
+        private void Sil_Click(object sender, RoutedEventArgs e)
+        {
+
+            int siraNo = listbox1.SelectedIndex;
+
+            if (siraNo > -1)
+
+            {
+
+                listbox1.Items.RemoveAt(siraNo);
+
+                MessageBox.Show("Sevgili öğrencimiz çıkışınız olmuştur :) ", "UYARI", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+            }
+
+
+            else
+            {
+
+                MessageBox.Show("Önce çıkarılacak elemanı seçiniz.", "HATA", MessageBoxButton.OK, MessageBoxImage.Error);
+
+
+            }
+
+        }
+
+        private void ad_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int u = ad.Text.Length;
+            if (u == 15)
+            {
+                MessageBox.Show("Maksimum sınıra ulaştınız!", "HATA", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void soyad_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int u = soyad.Text.Length;
+            if (u == 15)
+            {
+                MessageBox.Show("Maksimum sınıra ulaştınız!","HATA",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void tc_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int u = tc.Text.Length;
+            if (u == 11)
+            {
+                MessageBox.Show("Maksimum sınıra ulaştınız!", "HATA", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+        }
+
+        private void guncelle_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Guncelle gn = new Guncelle();
+
+            gn.ShowDialog();
+
+
+        }
+    }
+    }
